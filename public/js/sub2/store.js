6fabc67330c47ab5a1366a443dfd18d3a48c9d70
junilhwang <tjsdlf4261@naver.com>
2020-08-13 17:27:19 +0900

    refactor: [sub2] store 작성

---
+++ public/js/sub2/store.js
@@ -0,0 +1,27 @@
+import service from "./service";
+
+export default {
+  state: {
+    scrollData: JSON.parse(localStorage.getItem("scroll")) || 0,
+    buttonData: JSON.parse(localStorage.getItem("button")) || false,
+    items: [],
+    set scroll (value) {
+      this.scrollData = value;
+      localStorage.setItem('scroll', value);
+    },
+    set button (value) {
+      this.buttonData = value;
+      localStorage.setItem('button', value);
+    }
+  },
+  commit (key, value) {
+    this.state[key] = value;
+  },
+  async loadItems () {
+    const { statusCd, statusMsg, items } = await service.fetchCurrentExchangeRate();
+    if( statusCd !== 200 ){
+      throw statusMsg;
+    }
+    this.commit({ items });
+  }
+}
