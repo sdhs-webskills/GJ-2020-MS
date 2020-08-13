5d15e098b119d1544a37e769bd194c4f1a6b5cd0
junilhwang <tjsdlf4261@naver.com>
2020-08-13 17:00:37 +0900

    refactor: xml 분리

---
+++ public/js/xml.js
@@ -0,0 +1,23 @@
+import $ from './jquery-3.5.0.min';
+
+export default Object.freeze({
+  data: null,
+  async load () {
+    const xmlData = await fetch("/xml/festivalList.xml").then(v => $.parseXML(v.text()));
+
+    this.setData([ ...$(xmlData).find("item") ].map(({ children }) => {
+      return [ children ].reduce((acc, { tagName, children, textContent }) => {
+        acc[tagName] = tagName === "images" ?  [...children].map(v => v.textContent) :
+                       tagName === "sn"     ?  `00${textContent}`.substr(-3) :
+                       textContent;
+        return acc;
+      }, {});
+    }));
+  },
+  setData (data) {
+    this.data = data;
+  },
+  getData () {
+    return this.data;
+  },
+})
